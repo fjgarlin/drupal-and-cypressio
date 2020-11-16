d4495be80156aa27ee2b384f0fe06270a2257f62
Fran Garcia-Linares <fjgarlin@gmail.com>
2020-11-16 17:01:56 +0100

    Language test fix.

--- assets/d8-change-language.js
+++ assets/d8-change-language.js
@@ -2,7 +2,8 @@ describe('Changes language', () => {
     it('Ver las recetas', () => {
         cy.visit('/')
         cy.contains('Español').click()
+        cy.contains('Recetas').click()
         cy.url().should('include', '/es/')
-        cy.contains('Recetas')
+        cy.contains('Crema catalana').should('be.visible')
     })
 })
