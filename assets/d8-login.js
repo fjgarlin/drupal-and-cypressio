647e5034ea1a10618046fb406e24a9a00c61f1de
Fran Garcia-Linares <fjgarlin@gmail.com>
2020-11-17 15:06:59 +0100

    Change test and tweak D8 script.

--- assets/d8-login.js
+++ assets/d8-login.js
@@ -1,19 +1,10 @@
 describe('The Login Page', () => {
     it('sets auth cookie when logging in via form submission', function () {
       cy.visit('/user/login')
+      cy.get('input[name=name]').type('admin')
+      cy.get('input[name=pass]').type('admin{enter}')
   
-      cy.get('input[name=username]').type(username)
-  
-      // {enter} causes the form to submit
-      cy.get('input[name=password]').type(`${password}{enter}`)
-  
-      // we should be redirected to /dashboard
-      cy.url().should('include', '/dashboard')
-  
-      // our auth cookie should be present
-      cy.getCookie('your-session-cookie').should('exist')
-  
-      // UI should reflect this user being logged in
-      cy.get('h1').should('contain', 'jane.lane')
+      cy.url().should('include', '/user/1')
+      cy.get('h1').should('contain', 'admin')
     })
   })
--- d8-site.sh
+++ d8-site.sh
@@ -6,14 +6,17 @@ mkdir d8
 cd d8
 curl -sSL https://www.drupal.org/download-latest/tar.gz | tar -xz --strip-components=1
 
-# Launch server.
-nohup php core/scripts/drupal quick-start demo_umami &
-PHP_SERVER_PID=$!
+# Site install needs more than 128M
+echo "
+if (PHP_SAPI === 'cli') {
+  ini_set('memory_limit', '512M');
+}
+" >> sites/default/default.settings.php
 
 # Install CypressIO and create first test.
 echo "{}" > package.json
 echo '{
-    "baseUrl": "http://127.0.0.1:8888"
+    "baseUrl": "http://localhost:8888"
 }' > cypress.json
 npm install cypress --save-dev
 
@@ -21,10 +24,13 @@ npm install cypress --save-dev
 mkdir cypress && mkdir cypress/integration
 cp ../assets/* cypress/integration/
 
+# Launch server.
+# php -d memory_limit=512M composer require drush/drush:^9
+drush -y si demo_umami --db-url=sqlite://sites/default/files/.ht.sqlite --account-pass=admin
+nohup php -S localhost:8888 &
+PHP_SERVER_PID=$!
+
 npx cypress open
 
-
-
-# Quit server
+# Quit server.
 kill -9 $PHP_SERVER_PID
-
