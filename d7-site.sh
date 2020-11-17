54832f3d935f5dfd59e111d17ea040fcdb9c46b4
Fran Garcia-Linares <fjgarlin@gmail.com>
2020-11-17 10:54:13 +0100

    D7 script in place with test.

--- d7-site.sh
+++ d7-site.sh
@@ -3,30 +3,34 @@
 sudo rm -rf d7
 mkdir d7
 cd d7
-curl -sSL https://ftp.drupal.org/files/projects/drupal-7.73.tar.gz | tar -xz --strip-components=1
+git clone https://github.com/amazeeio/drupal7-example.git .
 
 # Launch server.
-# nohup php core/scripts/drupal quick-start demo_umami &
-# PHP_SERVER_PID=$!
+# pygmy up
+docker-compose up -d
+docker-compose exec cli drush -y si
 
-# # Install CypressIO and create first test.
-# echo "{}" > package.json
-# npm install cypress --save-dev
-# test="
-# describe('About page', () => {
-#     it('Goes to about page', () => {
-#         cy.visit('http://localhost:2020')
-#         //cy.visit('https://events.drupal.org/europe2020')
-#         cy.contains('About').click()
-#         cy.url().should('include', '/about')
-#     })
-# })
-# "
-# mkdir cypress && mkdir cypress/integration
-# touch cypress/integration/first_test.js
-# echo "$test" > cypress/integration/first_test.js
+# Install CypressIO and create first test.
+echo "{}" > package.json
+npm install cypress --save-dev
+test="
+describe('About page', () => {
+    it('Goes to about page', () => {
+        cy.visit('http://drupal7-example.docker.amazee.io')
+        cy.contains('Create new account').click()
+        cy.url().should('include', '/register')
+        cy.get('input[name=name]').type('Fran Garcia')
+        cy.get('input[name=mail]').type('fran.garcia@amazeelabs.com{enter}')
+        cy.contains('Thank you').should('be.visible')
+    })
+})
+"
+mkdir cypress && mkdir cypress/integration
+touch cypress/integration/user_register.js
+echo "$test" > cypress/integration/user_register.js
 
-# npx cypress open
+npx cypress open
 
-# # Quit server
-# kill -9 $PHP_SERVER_PID
+# Quit server
+docker-compose stop
+docker-compose down
