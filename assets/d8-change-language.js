27271b7aa4f785438501fbbd73e1f3605348ad83
Fran Garcia <fjgarlin@gmail.com>
2020-11-27 20:15:23 +0100

    Change test check.

--- assets/d8-change-language.js
+++ assets/d8-change-language.js
@@ -2,7 +2,7 @@ describe('Changes language', () => {
     it('Ver las recetas', () => {
         cy.visit('/')
         cy.contains('Español').click()
-        cy.url().should('include', '/es/')
+        cy.url().should('include', '/es')
         cy.contains('Recetas').click()
         cy.contains('Crema catalana').should('be.visible')
     })
